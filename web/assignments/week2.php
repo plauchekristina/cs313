a5e253af8124bc2ffb34cc66ef5772f1094d5994
kripl <kristinaplauche@gmail.com>
2018-09-26 02:14:16 -0400

    week 2 create sub directories

---
+++ web/assignments/week2.php
@@ -0,0 +1,52 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . '/common/nav.php');?> 
+<!---  ####### Body content begins ####### -->
+<div class="jumbotron">
+        <div class="container text-center">
+            <h1>Fall Semester 2018</h1>
+            <p>What I'm working on...</p>
+        </div>
+    </div>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-sm-12 panel panel-default text-left">
+                <div class="panel-body">
+                    <p> Hi</p>
+                </div>
+
+            </div>
+            <!-- Left column -->
+            <div class="col-sm-3 well">
+                <div class="well">
+                    <p>
+                        <a href="#">My Profile</a>
+                    </p>
+                    <img src="images/bushel-logo-gr-wh.svg" class="img-circle" height="65" width="65" alt="Avatar">
+                </div>
+
+
+            </div>
+
+            <!-- Center column -->
+            <div class="col-sm-9 well">
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="panel panel-default text-left">
+                            <div class="panel-body">
+                                <button>Click Me</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+    
+           
+        </div>
+
+    </div>
+
+ <?php include($_SERVER["DOCUMENT_ROOT"] . '/common/footer.php');?> 
---
+++ web/common/nav.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>CS 313 Kristina Plauché</title>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="../images/k-logo.svg">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="../index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="../week2.php">Assignments</a>
+                    </li>
+                    <li>
+                        <a href="#">About Me</a>
+                    </li>
+                </ul>
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group input-group">
+                        <input type="text" class="form-control" placeholder="Search..">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                    </div>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-user"></span> My Account</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+   
