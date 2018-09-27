0c63be54cfbd474fb5b4e01e5bdda96290350d29
kripl <kristinaplauche@gmail.com>
2018-09-27 01:05:25 -0400

    fixed nav image

--- web/common/nav.php
+++ web/common/nav.php
@@ -23,7 +23,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="#">
-                    <img src="../images/k-logo-small.svg">
+                    <img src="../images/k-logo-nav.svg">
                 </a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
