9e63fe510a231dcaefa96695e21933bdeac4592f
kripl <kristinaplauche@gmail.com>
2018-09-27 19:24:50 -0400

    adding new folders and files

---
+++ web/common/templates/template.php
@@ -0,0 +1,57 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . '/common/nav.php');?> 
+<!---  ####### Body content begins ####### -->
+<div class="jumbotron">
+        <div class="container text-center">
+            <h1>CS 313 - Fall 2018</h1>
+            <p>What I am doing...</p>
+        </div>
+    </div>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-sm-12 panel panel-default text-left">
+                <div  class="panel-body">
+                    <p></p>
+                   
+            </div>
+            </div>
+
+   
+
+            <!-- Left column -->
+            <div class="col-sm-3 well">
+                <div class="well">
+                    <h2>
+                        <a href="#">Kristina</a>
+</h2>
+                    <img src="../images/kp-profile.jpg" class="img-circle" height="65" width="65" alt="Avatar">
+<br><p> 
+                    <?php 
+                    echo "<strong>Today is:</strong> ".date("m/d/Y");?></p>
+                </div>
+
+
+            </div>
+
+            <!-- Center column -->
+            <div class="col-sm-9">
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="panel panel-default text-left well">
+                            <div class="panel-body">
+                            <h2>Assignments</h2>
+                            <p>Some content</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+<!--end center column-->
+</div> 
+</div>        
+       
+    <script src ="../scripts/functions.js"></script>
+ <?php include($_SERVER["DOCUMENT_ROOT"] . '/common/footer.php');?> 
