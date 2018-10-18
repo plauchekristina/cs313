c5323b8302144ec3ae5c27f68d64db79d8daa147
kripl <kristinaplauche@gmail.com>
2018-10-18 14:31:55 -0400

    moved around code on connect page

--- web/connect.php
+++ web/connect.php
@@ -3,4 +3,28 @@
 <html lang="en-us">
 <head><title>Connection</title></head>
 <body>
-<p>HI!</p></body></html>
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+<p>HI!</p></body>
+</html>
