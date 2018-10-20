0a06cd814f0c8d098800b33ca6f45af4b4fc46f8
kripl <kristinaplauche@gmail.com>
2018-10-20 12:21:47 -0400

    add home link to connection page

--- web/project/connection.php
+++ web/project/connection.php
@@ -26,13 +26,14 @@ catch (PDOException $ex)
 }
 ?>
 <p>The database is connected!</p>
+<a href="https://quiet-peak-48501.herokuapp.com/">Return to Home</a>
 <h2>Account List</h2>
 <ul>
 <?php
 
 	foreach ($db->query('SELECT first_name, last_name, username, email FROM account') as $row)
 		{
-		  echo "<li><b>Username:</b>" . $row['username'] . "<ul><li><b>Name:</b> ";
+		  echo "<li><b>Username: </b>" . $row['username'] . "<ul><li><b>Name:</b> ";
 		  echo $row['first_name'];
 		  echo " " . $row['last_name'] . " </li>";
 		  echo "<li><b>Email:</b>" . $row['email'] . "</li></ul>";
