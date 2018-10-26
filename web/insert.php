377a8d604eb41e4a2a32b0bb53bc4092e21fd8bb
kripl <kristinaplauche@gmail.com>
2018-10-26 13:47:33 -0400

    commented back in insert page

--- web/insert.php
+++ web/insert.php
@@ -13,16 +13,16 @@ echo "$scripture_verse";
 echo "$scripture_content";
 
 
+require('connect.php');
 
-
-//$db = get_db();
-//$stmt = $db->prepare ('INSERT INTO scriptures(scripture_book, scripture_chapter, scripture_verse, scripture_content ) VALUES (:scripture_book,
-//:scripture_chapter, :scripture_verse, :scripture_content');
-//$stmt -> bindValue(':scripture_book', $scripture_book, PDO::PARAM_STR);
-//$stmt -> bindValue(':scripture_chapter', $scripture_chapter, PDO::PARAM_INT);
-//$stmt -> bindValue(':scripture_verse', $scripture_verse, PDO::PARAM_INT);
-//$stmt -> bindValue(':scripture_content', $scripture_content, PDO::PARAM_STR);
-//$stmt->execute();
+$db = get_db();
+$stmt = $db->prepare ('INSERT INTO scriptures(scripture_book, scripture_chapter, scripture_verse, scripture_content ) VALUES (:scripture_book,
+:scripture_chapter, :scripture_verse, :scripture_content');
+$stmt -> bindValue(':scripture_book', $scripture_book, PDO::PARAM_STR);
+$stmt -> bindValue(':scripture_chapter', $scripture_chapter, PDO::PARAM_INT);
+$stmt -> bindValue(':scripture_verse', $scripture_verse, PDO::PARAM_INT);
+$stmt -> bindValue(':scripture_content', $scripture_content, PDO::PARAM_STR);
+$stmt->execute();
 
 
 
