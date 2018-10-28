8c221387aa0584f4be6f0d8af3b3bc707eca2798
kripl <kristinaplauche@gmail.com>
2018-10-27 23:37:58 -0400

    cleaned up connection page

--- web/project/connection.php
+++ web/project/connection.php
@@ -1,7 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="en-us">
-<head><title>Connection</title></head>
-<body>
+
 <?php
 try
 {
@@ -25,21 +22,3 @@ catch (PDOException $ex)
   die();
 }
 ?>
-<p>The database is connected!</p>
-<a href="https://quiet-peak-48501.herokuapp.com/">Return to Home</a>
-<h2>Account List</h2>
-<ul>
-<?php
-
-	foreach ($db->query('SELECT first_name, last_name, username, email FROM account') as $row)
-		{
-		  echo "<li><b>Username: </b>" . $row['username'] . "<ul><li><b>Name:</b> ";
-		  echo $row['first_name'];
-		  echo " " . $row['last_name'] . " </li>";
-		  echo "<li><b>Email:</b>" . $row['email'] . "</li></ul>";
-		  echo '</li>';
-    }
-  ?>
-  </ul>
-  </body>
-  </html>
