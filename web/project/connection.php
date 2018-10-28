b67999371c6c89644673458f0c43f1c0b04f6afd
kripl <kristinaplauche@gmail.com>
2018-10-27 23:27:28 -0400

    project connection page update

--- web/project/connection.php
+++ web/project/connection.php
@@ -3,8 +3,29 @@
 <head><title>Connection</title></head>
 <body>
 <?php
-require('connect.php');
 
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
 <p>The database is connected!</p>
 <a href="https://quiet-peak-48501.herokuapp.com/">Return to Home</a>
 <h2>Account List</h2>
