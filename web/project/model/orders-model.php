775188e89daf7f4136c7c15bab1bce0c07b903f1
kripl <kristinaplauche@gmail.com>
2018-10-29 14:03:09 -0400

    adding orders-model to site

---
+++ web/project/model/orders-model.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * Library of Functions
+ */
+require('../connection.php');
+var_dump($_POST);
+
+$orders_date= htmlspecialchars($_POST ['order-date']);
+$orders_total= htmlspecialchars($_POST ['orders_total']);
+$orders_full_qty= htmlspecialchars($_POST ['full-count']);
+$orders_full_budget= htmlspecialchars($_POST ['full-budget']);
+$orders_half_qty= htmlspecialchars($_POST ['half-count']);
+$orders_half_budget= htmlspecialchars($_POST ['half-budget']);
+
+   
+  
+
+      // The SQL statement
+      $sql = 'INSERT INTO orders (orders_date, orders_total, orders_full_qty, orders_half_qty, orders_full_budget, orders_half_budget)
+          VALUES (:orders_date, :orders_total, :orders_full_qty, :orders_half_qty, :orders_full_budget, :orders_half_budget)';
+      // Create the prepared statement using the db connection
+      $stmt = $db->prepare($sql);
+      // The next lines replace the placeholders in the SQL
+      // statement with the actual values in the variables
+      // and tells the database the type of data it is
+      $stmt->bindValue(':orders_date', $orders_date, PDO::PARAM_STR);
+      $stmt->bindValue(':orders_total', $orders_total, PDO::PARAM_STR);
+      $stmt->bindValue(':orders_full_qty', $orders_full_qty, PDO::PARAM_STR);
+      $stmt->bindValue(':orders_half_qty', $forders_half_qty, PDO::PARAM_INT);
+      $stmt->bindValue(':orders_full_budget', $orders_full_budget, PDO::PARAM_INT);
+      $stmt->bindValue(':orders_half_budget', $orders_half_budget, PDO::PARAM_INT);
+      $stmt->bindValue(':coop_half_budget', $half_budget, PDO::PARAM_INT);
+      // Insert the data
+      try {
+          $stmt->execute();
+          $last_id = $db->lastInsertId();
+      }
+         catch (Exception $e) {
+          echo $e;
+      }
+    
+
+    
+      $item_name= htmlspecialchars($_POST ['item-name']);
+      $item_count= htmlspecialchars($_POST ['item-count']);
+      $item_unit= htmlspecialchars($_POST ['item-unit']);
+      $item_price= htmlspecialchars($_POST ['item-price']);
+      $item_full_qty= htmlspecialchars($_POST ['per-full']);
+      $item_half_qty= htmlspecialchars($_POST ['per-half']);
+      $item_order_qty= htmlspecialchars($_POST ['item-quantity']);
+      $item_order_cost= htmlspecialchars($_POST ['item-total']);
+
+
+      // The SQL statement
+      $sql = 'INSERT INTO item (item_name, item_count, item_unit, item_price, item_full_qty, item_half_qty, item_order_qty, item_order_cost)
+      VALUES (:item_name, :item_count, :item_unit, :item_price, :item_full_qty, :item_half_qty, :item_order_qty, :item_order_cost)';
+  // Create the prepared statement using the db connection
+  $stmt = $db->prepare($sql);
+  // The next lines replace the placeholders in the SQL
+  // statement with the actual values in the variables
+  // and tells the database the type of data it is
+  $stmt->bindValue(':item_name', $item_name, PDO::PARAM_STR);
+  $stmt->bindValue(':orders_total', $item_count, PDO::PARAM_STR);
+  $stmt->bindValue(':item_unit', $item_unit, PDO::PARAM_STR);
+  $stmt->bindValue(':item_price', $item_price, PDO::PARAM_INT);
+  $stmt->bindValue(':item_full_qty', $item_full_qty, PDO::PARAM_INT);
+  $stmt->bindValue(':item_half_qty', $item_half_qty, PDO::PARAM_INT);
+  $stmt->bindValue(':item_order_qty', $item_order_qty, PDO::PARAM_INT);
+  $stmt->bindValue(':item_order_cost', $item_order_cost, PDO::PARAM_INT);
+  // Insert the data
+  try {
+      $stmt->execute();
+     
+  }
+     catch (Exception $e) {
+      echo $e;
+  }
+
+ 
+?>
