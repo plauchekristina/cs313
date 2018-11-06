32a3713859a833e87b58c8054786dd4a6503e515
kripl <kristinaplauche@gmail.com>
2018-11-06 00:38:12 -0500

    lets see if I can break the login page with this

--- web/project/login.php
+++ web/project/login.php
@@ -1,4 +1,5 @@
-<?php session_start();?>
+<?php session_start();
+require('model/login-model.php') ?>
 <?php include('../project/common/nav.php');?> 
 
 <!---  ####### Body content begins ####### -->
